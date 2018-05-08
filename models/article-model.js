5fd472d0415c681f5c7e5a1bdaa175aa469fd787
Jb mout <jeanbaptiste.mouterde@kedgebs.com>
2018-05-08 16:36:33 +0200

    model article

--- models/article-model.js
+++ models/article-model.js
@@ -2,11 +2,31 @@ const mongoose = require('mongoose');
 const Schema   = mongoose.Schema;
 
 const articleSchema = new Schema({
-  title: String,
-  date: String,
-  type: String,
+  title: { type: String }, 
+  date: { type: Date }, 
+  description: { type: String },
+  nameWritter: { type: String }, 
+  //GOOGLE MAP INTEGRATION
+  location: { type: String }, 
   email: String,
-});
+  imageUrl : {type : String}, 
+  organization: { 
+    type: String,
+    enum : ["World Wildlife Fund","Centra Terra Viva","Giz","World Ressource institute","Other"]
+  },
+  reportChange: { 
+    type: String,
+    enum : ["Increased rainfall","Decreased rainfall","Changes in timing of seasons","Storms","Loss of water source", "Wildlfires", "none"]
+  },
+  type: { 
+    type: String,
+    enum : ["Personal Information","Interview","Project","Story","Publication"]
+  },
+},
+{
+  timestamps : true
+}
+);
 
 const Article = mongoose.model('Article', articleSchema);
 module.exports = Article;
