1accac746308e984a0ced2aa6b9c2f7a4e2047db
Jb mout <jeanbaptiste.mouterde@kedgebs.com>
2018-05-17 16:59:15 +0200

    heroku2

--- routes/article-api-router.js
+++ routes/article-api-router.js
@@ -11,7 +11,7 @@ router.get("/articles", (req,res,next)=>{
   // find gives an Array
   Article
   .find()
-  .limit(6)
+  // .limit(6)
   .sort({createdAt: -1})
   .then((articles)=>{
     res.json(articles);
